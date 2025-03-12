4b277afcf07776ee94c6c89532a1a8ab2f150912
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-03-11 21:08:42 -0500

    primer ejercicio clase 2 terminado

--- class-2/src/App.jsx
+++ class-2/src/App.jsx
@@ -3,20 +3,24 @@ import './App.css'
 
 
 function App() {
-  const [a, setA] = useState(0)
-  const [b, setB] = useState(0)
-  const [c, setC] = useState(0)
-  
-  const add = () => {
-    setC(a + b)
+  const [iKing, setIKing] = useState(0)
+
+  const kings = [
+    {name: 'Ángel', hubby: 'programar'},
+    {name: 'David', hubby: 'tocar guitarra'},
+    {name: 'Alexander', hubby: 'montar patineta'}
+  ]
+
+  const nextKing = () => {
+    setIKing( iKing < kings.length-1 ? iKing+1 : 0 )
   }
 
   return (
     <div className="App">
-      <input type="number" value={a} onChange={e=>{setA(Number(e.target.value))}} /> +
-      <input type="number" value={b} onChange={e=>{setB(Number(e.target.value))}} /> =
-      <input type="number" value={c} readOnly />
-      <button onClick={add}>Sumar</button>
+      <div className="msg">
+        <p>La afición principal de <span className="darkviolet">{kings[iKing].name}</span> es <span className="green">{kings[iKing].hubby}</span></p>
+        <button onClick={nextKing}>Ver siguiente</button>
+      </div>
     </div>
   )
 }
