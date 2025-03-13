9064f06f75a3a9712ea30e024c00ccb7af659571
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-03-12 21:19:14 -0500

    Finalizando ejercicio de la clase 3

--- class-3/src/App.jsx
+++ class-3/src/App.jsx
@@ -1,13 +1,65 @@
-import MiComponente from './MiComponente'
+import { useState } from 'react'
+import Cena from './Cena'
 import './App.css'
 
 function App() {
+  const [total, setTotal] = useState(0)
+
+  const reyes = [
+    {
+      nombre: "Atanagildo",
+      color: "darkolivegreen",
+      precio: 178
+    }, {
+      nombre: "Ervigio",
+      color: "crimson",
+      precio: 169
+    }, {
+      nombre: "Ataúlfo",
+      color: "peru",
+      precio: 81
+    }, {
+      nombre: "Leogivildo",
+      color: "darkmagenta",
+      precio: 126
+    }, {
+      nombre: "Recesvinto",
+      color: "royalblue",
+      precio: 141
+    }, {
+      nombre: "Sisebuto",
+      color: "teal",
+      precio: 69
+    }
+  ]
+
+  let king
+
   return (
     <div className="App">
-      <h1>App</h1>
-      <MiComponente nombre="Ángel" edad={26} />
-      <MiComponente nombre="Sara" edad={24} />
-      <MiComponente edad={17} />
+      <h1>Cena con un rey</h1>
+      <p>Total: <span>{total}</span></p>
+
+      <div className="kings">      
+        {(()=>{king = reyes[0]})()}
+        <Cena name={king.nombre} price={king.precio} action={setTotal} color={king.color} />
+
+        {(()=>{king = reyes[1]})()}
+        <Cena name={king.nombre} price={king.precio} action={setTotal} color={king.color} />
+
+        {(()=>{king = reyes[2]})()}
+        <Cena name={king.nombre} price={king.precio} action={setTotal} color={king.color} />
+
+        {(()=>{king = reyes[3]})()}
+        <Cena name={king.nombre} price={king.precio} action={setTotal} color={king.color} />
+
+        {(()=>{king = reyes[4]})()}
+        <Cena name={king.nombre} price={king.precio} action={setTotal} color={king.color} />
+
+        {(()=>{king = reyes[5]})()}
+        <Cena name={king.nombre} price={king.precio} action={setTotal} color={king.color} />
+      </div>
+
     </div>
   )
 }
---
+++ class-3/src/Cena.jsx
@@ -0,0 +1,22 @@
+import './Cena.css'
+
+function MiComponente({ name="Anonymous", price=0, action=()=>{}, color }) {
+	const runAction = e => {
+		const divCena = e.target.parentElement
+		divCena.style.display = 'none'
+		action(total=>total+price)
+	}
+
+	const quitarAcentos = txt => txt.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+
+	return (
+		<div className="Cena" style={{background: color}}>
+			<h2>Rey {name}</h2>
+			<img src={`/reyes/rey_${quitarAcentos(name.toLowerCase())}.png`} alt={`Rey ${name}`} />
+			<p>Precio: <span>{price}</span></p>
+			<button onClick={runAction}>Comprar</button>
+		</div>
+	)
+}
+
+export default MiComponente
