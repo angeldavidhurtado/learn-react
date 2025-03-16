1f48e11459bea028fc0093c038f23cfd84542963
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-03-16 15:53:09 -0500

    Finalizando explicacion

--- class-5/src/App.jsx
+++ class-5/src/App.jsx
@@ -1,18 +1,26 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [name, setName] = useState('')
+  const [urlImg, setUrlImg] = useState('')
+
   useEffect(()=>{
     const urlApi = 'https://randomuser.me/api/'
     const res = fetch(urlApi)
     .then(res=>res.json())
-    .then(data=>console.log(data.results[0].name.first))
+    .then(data=>{
+      const user = data.results[0]
+      setName(`${user.name.first} ${user.name.last}`)
+      setUrlImg(user.picture.large)
+    })
     .catch(err=>console.log(err))
   }, [])
 
   return (
     <div className="App">
-      <p>Hola mundo !!!</p>
+      <img src={urlImg || null } alt={name} />
+      <p>{name}</p>
     </div>
   )
 }
