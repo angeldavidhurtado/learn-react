e7e2adb2c450e20125876441e29cd8c0d1819767
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-03-19 13:44:25 -0500

    Barra de navegacion

--- class-7/src/Nav.jsx
+++ class-7/src/Nav.jsx
@@ -1,6 +1,15 @@
+import { NavLink } from "react-router-dom"
+
 function Nav() {
 	return (
 		<nav>
+			<NavLink to="/">Inicio</NavLink>
+			<NavLink to="/atanagildo">Atanagildo</NavLink>
+			<NavLink to="/ataulfo">Ataulfo</NavLink>
+			<NavLink to="/ervigio">Ervigio</NavLink>
+			<NavLink to="/leovigildo">Leovigildo</NavLink>
+			<NavLink to="/recesvinto">Recesvinto</NavLink>
+			<NavLink to="/sisebuto">Sisebuto</NavLink>
 		</nav>
 	)
 }
