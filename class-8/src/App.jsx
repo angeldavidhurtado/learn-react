2f06776618379be09f47cc625a4fdcefbe0c04c9
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-03-22 13:42:46 -0500

    Iniciando ejercicio clase 8

--- class-8/src/App.jsx
+++ class-8/src/App.jsx
@@ -1,8 +1,22 @@
+import { useReducer } from 'react'
 import './App.css'
 
+const reducer = () => {}
+const initialListState = []
+
 function App() {
+  const [listState, dispatch] = useReducer(reducer, initialListState)
+
   return (
     <>
+      <div className="agregar-producto">
+        <span>Producto</span>
+        <input type="text" />
+        <button>Agregar</button>
+      </div>
+      <div className="productos">
+
+      </div>
     </>
   )
 }
