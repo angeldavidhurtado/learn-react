e1b4adad4c8b743e3a2b799daa6665ec536d4c59
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-03-28 21:22:15 -0500

    agregando funcionalidad de login

--- class-9/src/App.jsx
+++ class-9/src/App.jsx
@@ -4,21 +4,34 @@ import Login from './Login'
 import Places from './Places'
 import SiteDetail from './SiteDetail'
 import MyTravelPlan from './MyTravelPlan'
+import { createContext, useState } from 'react'
+
+export const AuthContext = createContext()
+export const r = Math.random() // importo este valor en Login.jsx
+console.log(`random App: ${r}`)
+console.log('-----------------------')
+console.log(AuthContext)
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Login />} />
-        <Route path="/login" element={<Navigate to="/" replace />} />
+  const [user, setUser] = useState(null)
+  const login = userName => setUser(userName)
+  const logout = () => setUser(null)
 
-        <Route element={<LayoutWithNav />}>
-          <Route path="/area/:area" element={<Places />} />
-          <Route path="/site" element={<SiteDetail />} />
-          <Route path="/plan" element={<MyTravelPlan />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Login />} />
+          <Route path="/login" element={<Navigate to="/" replace />} />
+
+          <Route element={<LayoutWithNav />}>
+            <Route path="/area/:area" element={<Places />} />
+            <Route path="/site" element={<SiteDetail />} />
+            <Route path="/plan" element={<MyTravelPlan />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   )
 }
 
--- class-9/src/Login.jsx
+++ class-9/src/Login.jsx
@@ -1,10 +1,29 @@
+import { useContext, useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AuthContext } from './App'
+import { r } from './App'
+
 function Login() {
+  const { login } = useContext(AuthContext)
+  const inputUserNameRef = useRef(null)
+  const navigate = useNavigate()
+
+  console.log(`random Login ${r}`)
+
+  const buttonLogin = () => {
+    let userName = inputUserNameRef.current.value
+    userName = userName.replace(/\s+/g, ' ').trim()
+    if (!userName) return
+    login(userName)
+    navigate('/area/cp')
+  }
+
 	return (
 		<div className="Login">
       <h1>Vive el país</h1>
       <label htmlFor="user">Nombre</label>
-      <input type="text" id="user" />
-      <button>Login</button>
+      <input ref={inputUserNameRef} type="text" id="user" />
+      <button onClick={buttonLogin}>Login</button>
     </div>
 	)
 }
