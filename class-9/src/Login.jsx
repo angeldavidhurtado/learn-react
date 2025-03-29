4aabff397a5d7649812b0906fcbcca5078f08a32
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-03-28 22:02:41 -0500

    Dando estilos al login

--- class-9/src/Login.jsx
+++ class-9/src/Login.jsx
@@ -1,27 +1,32 @@
-import { useContext, useRef } from 'react'
+import { useContext, useRef, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from './App'
 import { r } from './App'
+import './Login.css'
 
 function Login() {
-  const { login } = useContext(AuthContext)
+  const { user, login } = useContext(AuthContext)
   const inputUserNameRef = useRef(null)
   const navigate = useNavigate()
 
   console.log(`random Login ${r}`)
+
+  useEffect(() => {
+    if (user) navigate('/area/cp', {replace: true})
+  }, [])
 
   const buttonLogin = () => {
     let userName = inputUserNameRef.current.value
     userName = userName.replace(/\s+/g, ' ').trim()
     if (!userName) return
     login(userName)
-    navigate('/area/cp')
+    navigate('/area/cp', {replace: true})
   }
 
 	return (
-		<div className="Login">
+		<div className="login">
       <h1>Vive el país</h1>
-      <label htmlFor="user">Nombre</label>
+      <label htmlFor="user">Nombre de usuario</label>
       <input ref={inputUserNameRef} type="text" id="user" />
       <button onClick={buttonLogin}>Login</button>
     </div>
