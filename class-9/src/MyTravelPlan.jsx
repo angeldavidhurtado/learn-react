723c516ac721625624ce29db6fdcb252390d8491
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-04-01 12:15:25 -0500

    Mejorando funcionalidad de ver lugares seleccionados

--- class-9/src/MyTravelPlan.jsx
+++ class-9/src/MyTravelPlan.jsx
@@ -4,12 +4,14 @@ import './MyTravelPlan.css'
 
 function MyTravelPlan() {
 	const { user, plansToBuy } = useContext(AuthContext)
+	const places = plansToBuy.state.placesBuy
 
 	return (
 		<div className="my-travel-plan">
 			<h1>Lugares a visitar</h1>
 			<ul>
-				{plansToBuy.state.placesBuy.map(place =>
+				{places.length ?
+				places.map(place =>
 					<li key={place.name}>
 						<p>{place.name} (${place.cost})</p>
 						<button onClick={
@@ -19,7 +21,9 @@ function MyTravelPlan() {
 							})
 						}>Anular</button>
 					</li>
-				)}
+				)
+				: <li><p>No has seleccionado lugares</p></li>
+				}
 			</ul>
 			<p className="cost">Precio total: ${plansToBuy.state.total}</p>
 			<p className="user-name">Usuario: {user}</p>
