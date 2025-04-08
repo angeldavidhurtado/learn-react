ad76ef47bb67afa888bf4ea982b0bbb30ef83c9b
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-04-08 15:16:55 -0500

    Agregando datos de la practica

---
+++ class-10/src/data.js
@@ -0,0 +1,22 @@
+const places = [
+	{
+		place: 'Marítima',
+		cost: 210,
+		img: 'zonaMaritima.jpg'
+	},
+	{
+		place: 'Ciudades',
+		cost: 175,
+		img: 'zonaCiudad.jpg'
+	},
+	{
+		place: 'Naturales',
+		cost: 154,
+		img: 'zonaNatural.webp'
+	},
+	{
+		place: 'de Riesgo',
+		cost: 52,
+		img: 'zonaAdrenalina.jpg'
+	}
+]
