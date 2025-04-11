d056dcfee106dee8c9b0cad26cc6bc5bdfe9da6f
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-04-10 22:06:30 -0500

    Iniciando la seccion de la secuencia de preguntas

--- class-10/src/pages/form-questions/FormQuestions.jsx
+++ class-10/src/pages/form-questions/FormQuestions.jsx
@@ -1,7 +1,22 @@
+import formQuestions from './../../data/formQuestions'
+import dataFormContext from './../../context/dataFormContext'
+import { useContext } from 'react'
+import { Navigate } from 'react-router-dom'
+
 function FormQuestions() {
+	const {
+		stateIndexQuestionForm,
+		formAnswers
+	} = useContext(dataFormContext)
+
+	const iQuestion = stateIndexQuestionForm.value
+	if (iQuestion < 3) return <Navigate to="/" replace />
+
+	const question = formQuestions[iQuestion].question
+
 	return (
 		<p>
-			asdf
+			{question}
 		</p>
 	)
 }
