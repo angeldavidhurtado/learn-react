caefe518d2cf7507263b18e62f386f1f2c32bdee
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-04-12 15:59:44 -0500

    Funcionalidad de mostrar todas las preguntas secuencialmente

--- class-10/src/data/formQuestions.js
+++ class-10/src/data/formQuestions.js
@@ -8,6 +8,7 @@ const formQuestions = [
 		question: '¿Cual es tu nombre?',
 		watchMessage: 'como nombre de usuario',
 		required: true,
+		type: 'text',
 		min: null,
 		max: null
 	},
@@ -15,6 +16,7 @@ const formQuestions = [
 		question: '¿Cuantas habitaciones quieres?',
 		watchMessage: 'habitaciones',
 		required: true,
+		type: 'number',
 		min: 1,
 		max: 10
 	},
@@ -22,6 +24,7 @@ const formQuestions = [
 		question: '¿Cuantas personas son?',
 		watchMessage: 'personas',
 		requiered: true,
+		type: 'number',
 		min: 1,
 		max: 7
 	},
@@ -29,6 +32,7 @@ const formQuestions = [
 		question: '¿Para cuantos días?',
 		watchMessage: 'días',
 		required: true,
+		type: 'number',
 		min: 1,
 		max: 7
 	},
--- class-10/src/pages/form-questions/FormQuestions.jsx
+++ class-10/src/pages/form-questions/FormQuestions.jsx
@@ -1,6 +1,7 @@
 import formQuestions from './../../data/formQuestions'
 import dataFormContext from './../../context/dataFormContext'
 import { useContext } from 'react'
+import { useForm } from 'react-hook-form'
 import { Navigate } from 'react-router-dom'
 
 function FormQuestions() {
@@ -12,12 +13,52 @@ function FormQuestions() {
 	const iQuestion = stateIndexQuestionForm.value
 	if (iQuestion < 3) return <Navigate to="/" replace />
 
-	const question = formQuestions[iQuestion].question
+	const question = formQuestions[iQuestion]
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		setFocus,
+		reset,
+		watch
+	} = useForm()
+
+	const validateForm = data => {
+		stateIndexQuestionForm.set(iQuestion + 1)
+		formAnswers.push(data.value)
+		setFocus('value')
+		reset()
+		console.log(formAnswers)
+	}
 
 	return (
-		<p>
-			{question}
-		</p>
+		<form onSubmit={handleSubmit(validateForm)}>
+			<p>{question.question}</p>
+			<input
+				type={question.type}
+				{...register('value', {
+					required: question.required,
+					min: question.min,
+					max: question.max
+				})}
+				autoFocus
+				autoComplete="off"
+			/>
+			<input type="submit" />
+			{
+				errors.value?.type == 'required' &&
+				<p>Por favor completa el campo</p>
+			}
+			{
+				errors.value?.type == 'min' &&
+				<p>Debe ser un número mayor</p>
+			}
+			{
+				errors.value?.type == 'max' &&
+				<p>Debe ser un número menor</p>
+			}
+		</form>
 	)
 }
 
