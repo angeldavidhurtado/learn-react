720757ce164a16622881a220792eefe8e4981716
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-04-14 19:05:04 -0500

    Agregando campos al resumen

--- class-10/src/components/form-summary/FormSummary.jsx
+++ class-10/src/components/form-summary/FormSummary.jsx
@@ -1,7 +1,25 @@
+import dataFormContext from './../../context/dataFormContext'
+import { useContext } from 'react'
+
 function FormSummary() {
+	const fields = [
+		'Imagen',
+		'Zona',
+		'$ por día',
+		'Nombre',
+		'Habitaciones',
+		'Personas',
+		'Días'
+	]
+	const { formAnswers } = useContext(dataFormContext)
+
 	return (
 		<>
-			Resumen
+			{formAnswers.map((answer, iAnswer) =>
+				<div key={iAnswer}>
+					{answer} {iAnswer}
+				</div>
+			)}
 		</>
 	)
 }
