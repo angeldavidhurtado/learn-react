c364376b6b774c2fbd9353831d15537268a660c6
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-04-19 21:00:19 -0500

    Experimento para servir archivos estaticos con github pages, vite y react

--- class-8/vite.config.js
+++ class-8/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/learn-react/class-8',
+  base: '/learn-react/class-8/',
   plugins: [react()],
+  preview: {
+    historyApiFallback: true
+  }
 })
