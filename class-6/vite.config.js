1f4f793200e4adf8a80b457892eb8dcb7509649d
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-04-20 14:52:33 -0500

    Finalizando actualizacion de rutas y deploy

--- class-6/vite.config.js
+++ class-6/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/learn-react/class-6',
+  base: '/learn-react/class-6/',
   plugins: [react()],
 })
